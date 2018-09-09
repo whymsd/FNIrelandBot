7269934f8d342d7dc04c0197b90aa9cdb4eb8c0f
whymsd <whymsd@tcd.ie>
2018-09-09 18:51:36 +0100

    Now implements basic stat delivery.

--- bot.js
+++ bot.js
@@ -41,19 +41,30 @@ client.on("message", function (user, userID, channelID, message, evt) {
             case "help":
                 client.sendMessage({
                     to: channelID,
-                    message: "```-customs\t\tInformation on the Fortnite Ireland Community games.\n-stats  \t\tGet Whymsy's stats to the console.```"
+                    message: "```-customs \t\t\t\t\t\tInformation on the Fortnite Ireland Community games.\n-solos <Username> <Platform>\t Get the solos stats for a user.\n-duos <Username> <Platform>\t  Get the duos stats for a user.\n-squads <Username> <Platform>\tGet the squads stats for a user.```"
                 })
                 console.log(args[1]);
             break;
-            case "stats":
+            case "solos":
+            case "duos":
+            case "squads":
+                console.log(args[2]);
                 if(typeof(args[1]) == "undefined"){
                     client.sendMessage({
                         to: channelID,
                         message: "Please enter an Epic ID to get stats for!"
                     });
                 }
+                /*else if(args[2] != "pc" && args[2] != "ps4" && args[2] != "xb1" && typeof(args[2]) != undefined){
+                    client.sendMessage({
+                        to: channelID,
+                        message: "Platform entered was invalid"
+                    });
+                }*/
                 else{
-                    fortnite.user("Whymsy", "pc").then(console.log); // this is the actual call for stats, it's actually so easy woo
+                    //var myUser;
+                    statDelivery(args[1], args[2], cmd, channelID);
+                    
                 }
             break;
             case "test":
@@ -72,4 +83,40 @@ client.on("message", function (user, userID, channelID, message, evt) {
      }
 });
 
-
+function statDelivery(user, platform = "pc", mode, channelID){
+    fortnite.user(user, platform).then((myUser) =>{
+        //console.log("WE HERE: " + myUser.username);
+        var myMode
+        switch(mode){
+            case "solos":
+                myMode = myUser.stats.solo;
+            break;
+            case "duos":
+                myMode = myUser.stats.duo;
+            break;
+            case "squads":
+                myMode = myUser.stats.squad;
+            break;
+        }
+        if(typeof(myMode) === "undefined"){
+            client.sendMessage({
+                to: channelID,
+                message: "```User has no stats for this game mode!```"
+            });
+        }
+        else{
+            console.log(myMode.wins);
+            client.sendMessage({
+                to: channelID,
+                message: "```Username: " + myUser.username + "\nMatches: " + myMode.matches + "\nWins: " + myMode.wins + "\nKills: " + myMode.kills + "```"
+            });
+        }
+    }).catch((error) => {
+        client.sendMessage({
+            to: channelID,
+            message: "```Error retrieving stats, please try again later.```"
+        });
+        console.log("Error: " + error);
+    });
+    console.log("Mission complete.");
+}
